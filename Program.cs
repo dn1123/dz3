5c6abf51baeb4a4000cbce0106194727788e096b
Dmitry <dn1123667@yandex.ru>
2022-09-27 17:34:28 +0300

    zad19+2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+string InNumber()
+{
+    while (true)
+    {
+        string = = Console.ReadLine();
+        if (int.TryParse(text, out int ))
+        {
+            if (text.Length == 5)
+            {
+                Console.WriteLine($"Вы ввели число: {text}");
+
+                return ;
+
+                break;
+            }
+            else
+            {
+                Console.Write("Вы ввели не пятизначное число. \nВведите число ещё раз: ");
+            }
+        }
+        else
+        {
+            Console.Write("Не удалось распознать число. \nВведите число ещё раз: ");
+        }
+    }
+}
+
+Console.Write("Введите пятизначное число: ");
+
+string = = InNumber();
+
+if (stringNumber[0] == stringNumber[4] || stringNumber[1] == stringNumber[3])
+{
+    Console.WriteLine($"Ваше число: {stringNumber} - палиндром.");
+}
+else Console.WriteLine($"Ваше число: {stringNumber} - НЕ палиндром.");
