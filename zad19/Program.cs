cbfc19c973fe731fe9ce8bf105ea25046f5ab307
dn1123 <112434391+dn1123@users.noreply.github.com>
2022-09-30 17:55:19 +0300

    Add files via upload

---
+++ zad19/Program.cs
@@ -0,0 +1,36 @@
+﻿string InNumber()
+{
+    while (true)
+    {
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+            if (text.Length == 5)
+            {
+                Console.WriteLine($"Вы ввели число: {text}");
+
+                return text;
+
+                break;
+            }
+            else
+            {
+                Console.Write("Вы ввели не пятизначное число. \nВведите число ещё раз: ");
+            }
+        }
+        else
+        {
+            Console.Write("Не удалось распознать число. \nВведите число ещё раз: ");
+        }
+    }
+}
+
+Console.Write("Введите пятизначное число: ");
+
+string stringNumber = InNumber();
+
+if (stringNumber[0] == stringNumber[4] || stringNumber[1] == stringNumber[3])
+{
+    Console.WriteLine($"Ваше число: {stringNumber} - палиндром.");
+}
+else Console.WriteLine($"Ваше число: {stringNumber} - НЕ палиндром.");
---
+++ zad23/Program.cs
@@ -0,0 +1,28 @@
+﻿int InNumber()
+{
+    while (true)
+    {
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+
+            Console.WriteLine($"Вы ввели число: {number}");
+
+            return number;
+        }
+        else
+        {
+            Console.Write("Не удалось распознать число. \nВведите число ещё раз: ");
+        }
+    }
+}
+
+Console.Write("Введите число: ");
+
+int cube = InNumber();
+
+for(int i =1; i <= cube; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+
+}
